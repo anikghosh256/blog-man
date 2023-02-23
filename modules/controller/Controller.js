c602cafe24c7504bbd61baabb05c48707bef17ba
Anik Ghosh <anikghosh46.bd@gmail.com>
2023-02-24 01:10:56 +0600

    Response method updated.

--- modules/controller/Controller.js
+++ modules/controller/Controller.js
@@ -1,8 +1,20 @@
 export default class Controller{
-  response(data, httpCode = 200) {
+  success(data, httpCode = 200) {
     return {
       statusCode: httpCode,
       body: JSON.stringify(data),
+    };
+  }
+
+  error(data, httpCode = 500) {
+    const error = new Error(data);
+    return {
+      statusCode: httpCode,
+      body: JSON.stringify({
+        name: error.name,
+        error: error.message,
+        stack: error.stack,
+      }),
     };
   }
 }
